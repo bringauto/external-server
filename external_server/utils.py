16406c020742b3feee893fe55c9faed20b58f73f
Jiri Zak <jiri.zak@bringauto.com>
2022-12-29 10:24:52 +0100

    update help message in arguments

--- external_server/utils.py
+++ external_server/utils.py
@@ -8,10 +8,10 @@ def argparse_init() -> argparse.Namespace:
                         default='127.0.0.1', help='ip address of the MQTT server')
     parser.add_argument('-p', '--port', type=int, default=1883,
                         help='port of the MQTT server')
-    parser.add_argument('--tls', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--ca', type=str)
-    parser.add_argument('--cert', type=str)
-    parser.add_argument('--key', type=str)
+    parser.add_argument('--tls', action=argparse.BooleanOptionalAction, help='Tls authentication')
+    parser.add_argument('--ca', type=str, help='path to ca certification, used if tls is set')
+    parser.add_argument('--cert', type=str, help='path to cert file, used if tls is set')
+    parser.add_argument('--key', type=str, help='path to key file, used if tls is set')
     return parser.parse_args()
 
 
