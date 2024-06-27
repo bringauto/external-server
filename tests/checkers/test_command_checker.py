f4ad4d31067f7a539b97d22dd4b06385c53697b6
jiri.strouhal <jiri.strouhal@gmail.com>
2024-06-27 14:40:58 +0200

    Rename test module command_checker to checkers

--- tests/checkers/test_command_checker.py
+++ tests/checkers/test_command_checker.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import sys
 import logging
+
 sys.path.append("lib/fleet-protocol/protobuf/compiled/python")
 
 from ExternalProtocol_pb2 import Command
@@ -40,29 +41,34 @@ class Test_Pop_Command(unittest.TestCase):
         self.assertEqual(self.checker.acknowledge_and_pop_commands(msg_counter=1), [])
         self.assertEqual(self.checker.acknowledge_and_pop_commands(msg_counter=2), [])
 
-    def test_with_matching_msg_counter_yields_all_previously_commands_to_be_popped_without_maching_msg_counter(self):
+    def test_with_matching_msg_counter_yields_all_previously_commands_to_be_popped_without_maching_msg_counter(
+        self,
+    ):
         self.checker.acknowledge_and_pop_commands(msg_counter=1)
         self.checker.acknowledge_and_pop_commands(msg_counter=2)
         # the correct message counter value is 0, after popping cmd with this counter value
         # all three popped commands are returned in the correct order
         self.assertEqual(
             self.checker.acknowledge_and_pop_commands(msg_counter=0),
-            [(self.cmd_2, False), (self.cmd_1, False), (self.cmd_3, False)]
+            [(self.cmd_2, False), (self.cmd_1, False), (self.cmd_3, False)],
         )
 
-    def test_with_matching_msg_counter_is_not_affected_if_some_command_popped_in_wrong_order_were_already_returned(self):
+    def test_with_matching_msg_counter_is_not_affected_if_some_command_popped_in_wrong_order_were_already_returned(
+        self,
+    ):
         self.checker.acknowledge_and_pop_commands(msg_counter=1)
         self.assertEqual(
             self.checker.acknowledge_and_pop_commands(msg_counter=0),
-            [(self.cmd_2, False), (self.cmd_1, False)]
+            [(self.cmd_2, False), (self.cmd_1, False)],
         )
         # previously, all commands that were popped in wrong order were returned and the next command
         # popped with matching msg_counter is returned as usual
-        self.assertEqual(self.checker.acknowledge_and_pop_commands(msg_counter=2), [(self.cmd_3, False)])
+        self.assertEqual(
+            self.checker.acknowledge_and_pop_commands(msg_counter=2), [(self.cmd_3, False)]
+        )
 
     def test_with_counter_not_in_receivedf_acks_yields_empty_list(self):
         self.assertEqual(self.checker.acknowledge_and_pop_commands(msg_counter=3), [])
-
 
 
 class Test_Exceeding_Timeout_For_Commands(unittest.TestCase):
@@ -85,29 +91,28 @@ class Test_Exceeding_Timeout_For_Commands(unittest.TestCase):
 
     def test_timeout_is_not_set_when_command_is_acknowledged(self):
         self.assertFalse(self.checker.timeout.is_set())
-        time.sleep(CHECKER_TIMEOUT/2)
+        time.sleep(CHECKER_TIMEOUT / 2)
         self.checker.acknowledge_and_pop_commands(msg_counter=0)
-        time.sleep(CHECKER_TIMEOUT/2)
+        time.sleep(CHECKER_TIMEOUT / 2)
         self.assertFalse(self.checker.timeout.is_set())
 
     def test_timeout_is_not_set_when_all_commands_are_acknowledged(self):
         self.checker.add_command(Command(), False)
         self.assertFalse(self.checker.timeout.is_set())
-        time.sleep(CHECKER_TIMEOUT/2)
+        time.sleep(CHECKER_TIMEOUT / 2)
         self.checker.acknowledge_and_pop_commands(msg_counter=0)
         self.checker.acknowledge_and_pop_commands(msg_counter=1)
-        time.sleep(CHECKER_TIMEOUT/2 + 0.1)
+        time.sleep(CHECKER_TIMEOUT / 2 + 0.1)
         self.assertFalse(self.checker.timeout.is_set())
 
     def test_timeout_is_set_when_any_command_is_not_acknowledged(self):
         self.checker.add_command(Command(), False)
         self.assertFalse(self.checker.timeout.is_set())
-        time.sleep(CHECKER_TIMEOUT/2)
+        time.sleep(CHECKER_TIMEOUT / 2)
         self.checker.acknowledge_and_pop_commands(msg_counter=0)
-        time.sleep(CHECKER_TIMEOUT/2 + 0.1)
+        time.sleep(CHECKER_TIMEOUT / 2 + 0.1)
         self.assertTrue(self.checker.timeout.is_set())
 
 
-
-if __name__=="__main__":  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     unittest.main()
