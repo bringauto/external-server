7833dd28ab22b4aaf207cbcb0189bc83a9229cc6
jiri.strouhal <jiri.strouhal@gmail.com>
2024-06-28 09:17:23 +0200

    Rewrite tests for the session timeout checker

---
+++ tests/checkers/test_session_timeout_checker.py
@@ -0,0 +1,55 @@
+import unittest
+import time
+import sys
+sys.path.append("lib/fleet-protocol/protobuf/compiled/python")
+
+from external_server.checker.session_timeout_checker import SessionTimeoutChecker
+
+
+_CHECKER_TIMEOUT = 0.05
+
+
+class Test_Setting_Timeout(unittest.TestCase):
+
+    def test_started_checker_does_set_timeout(self):
+        checker = SessionTimeoutChecker(_CHECKER_TIMEOUT)
+        checker.start()
+        time.sleep(_CHECKER_TIMEOUT)
+        self.assertTrue(checker.timeout.is_set())
+
+    def test_starting_checker_again_has_no_effect(self):
+        checker = SessionTimeoutChecker(_CHECKER_TIMEOUT)
+        checker.start()
+        time.sleep(_CHECKER_TIMEOUT/2)
+        checker.start()
+        time.sleep(_CHECKER_TIMEOUT/2+.001)
+        self.assertTrue(checker.timeout.is_set())
+
+    def test_stopped_checker_does_not_set_timeout(self):
+        checker = SessionTimeoutChecker(_CHECKER_TIMEOUT)
+        checker.start()
+        checker.stop()
+        time.sleep(_CHECKER_TIMEOUT + 0.01)
+        self.assertFalse(checker.timeout.is_set())
+
+    def test_stopping_checker_after_timeout_unsets_the_timeout(self):
+        checker = SessionTimeoutChecker(_CHECKER_TIMEOUT)
+        checker.start()
+        time.sleep(_CHECKER_TIMEOUT + 0.01)
+        self.assertTrue(checker.timeout.is_set())
+        checker.stop()
+        self.assertFalse(checker.timeout.is_set())
+
+    def test_restarting_checker_postpones_setting_timeout(self):
+        checker = SessionTimeoutChecker(_CHECKER_TIMEOUT)
+        checker.start()
+        time.sleep(_CHECKER_TIMEOUT/2)
+        checker.reset()
+        time.sleep(_CHECKER_TIMEOUT/2)
+        self.assertFalse(checker.timeout.is_set())
+        time.sleep(_CHECKER_TIMEOUT/2+.001)
+        self.assertTrue(checker.timeout.is_set())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
