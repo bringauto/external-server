66b185b0fe91894921aa69f39c2574e8d4b622c9
jiri.strouhal <jiri.strouhal@gmail.com>
2024-10-09 10:18:48 +0200

    Change order of imports for better readability

--- external_server/checkers/command_checker.py
+++ external_server/checkers/command_checker.py
@@ -5,16 +5,15 @@ import dataclasses
 
 sys.path.append("lib/fleet-protocol/protobuf/compiled/python")
 
-from external_server.checkers.checker import Checker as _Checker
-from external_server.logs import CarLogger as _CarLogger, LOGGER_NAME
-from external_server.logs import CarLogger as _CarLogger
 from ExternalProtocol_pb2 import CommandResponse as _CommandResponse  # type: ignore
+from InternalProtocol_pb2 import Device as _Device  # type: ignore
+from external_server.checkers.checker import TimeoutChecker as _Checker
+from external_server.logs import CarLogger as _CarLogger, LOGGER_NAME
 from external_server.models.structures import (
     Counter as _Counter,
     HandledCommand as _HandledCommand,
     TimeoutType as _TimeoutType,
 )
-from InternalProtocol_pb2 import Device as _Device  # type: ignore
 from external_server.models.events import EventQueue as _EventQueue
 
 
