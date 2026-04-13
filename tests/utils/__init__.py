e0005fe97b57b0453f2516221ce710cd4a6250a7
jiri-skuta <jiri.skuta@bringauto.com>
2026-04-13 22:16:00 +0200

    BAF-1404: update ServerConfig to use external_communication block

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -19,24 +19,34 @@ EXAMPLE_MODULE_SO_LIB_PATH: FilePath = FilePath(
 )
 
 
-COMMON_CONFIG = {
-    "mqtt_address": "127.0.0.1",
-    "mqtt_port": 1883,
-    "mqtt_timeout": 2,
+_COMMON_NON_MQTT_CONFIG = {
     "timeout": 2,
     "send_invalid_command": False,
     "sleep_duration_after_connection_refused": 2,
-    "log_files_directory": ".",
-    "log_files_to_keep": 5,
-    "log_file_max_size_bytes": 100000,
     "logging": {
         "console": {"level": "DEBUG", "use": True},
         "file": {"level": "DEBUG", "use": True, "path": "./log"},
     },
 }
 
+COMMON_CONFIG = {
+    "mqtt_address": "127.0.0.1",
+    "mqtt_port": 1883,
+    "mqtt_timeout": 2,
+    **_COMMON_NON_MQTT_CONFIG,
+}
+
 CAR_CONFIG_WITHOUT_MODULES = {"company_name": "ba", "car_name": "car1", **COMMON_CONFIG}
-ES_CONFIG_WITHOUT_MODULES = {"company_name": "ba", **COMMON_CONFIG}
+ES_CONFIG_WITHOUT_MODULES = {
+    "company_name": "ba",
+    "external_communication": {
+        "protocol": "MQTT",
+        "server_address": "127.0.0.1",
+        "server_port": 1883,
+        "timeout_ms": 2000,
+    },
+    **_COMMON_NON_MQTT_CONFIG,
+}
 
 
 def get_test_server(
@@ -47,7 +57,7 @@ def get_test_server(
     config = _ServerConfig(common_modules={"1000": module_config}, **ES_CONFIG_WITHOUT_MODULES, cars=cars)  # type: ignore
     config.company_name = company
     if mqtt_timeout > 0:
-        config.mqtt_timeout = mqtt_timeout
+        config.external_communication.timeout_ms = int(mqtt_timeout * 1000)
     if timeout > 0:
         config.timeout = timeout
     es = ExternalServer(config=config)
